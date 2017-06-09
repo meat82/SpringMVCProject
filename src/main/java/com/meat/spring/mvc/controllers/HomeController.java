62b14ab4d905c4e509991a9bc397224210b103bc
Mikko Torvinen <mikko.torvinen@ideal.fi>
2017-06-09 09:31:18 +0300

    remove gwt
    Remove gwt implementation from spring project

--- src/main/java/com/meat/spring/mvc/controllers/HomeController.java
+++ src/main/java/com/meat/spring/mvc/controllers/HomeController.java
@@ -14,7 +14,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
